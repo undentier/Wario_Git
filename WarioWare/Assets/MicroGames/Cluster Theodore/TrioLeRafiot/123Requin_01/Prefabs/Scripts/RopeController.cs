4b6a7ed55aba7999c1b5489d21a5da0545fcb30a
Guillaume <g.roge@rubika-edu.com>
2020-12-01 17:20:04 +0100

    Rangement unity

---
+++ WarioWare/Assets/MicroGames/Cluster Theodore/TrioLeRafiot/123Requin_01/Prefabs/Scripts/RopeController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// by Antoine
+/// This script ise use to pull up by x height the rope of y height
+/// </summary>
+
+public class RopeController : MonoBehaviour
+{
+    #region Variables
+    private LineRenderer rope;
+    private float timer = 0;
+    private bool win;
+
+    [Header("Object attached")]
+    public GameObject attachedTo;
+
+    [Header ("Rope Settings")]
+    public int ropeSize = 10;
+    public int pullingUpRopeSize = 1;
+
+    [Header("Level 3 Parameters")]
+    public bool level3;
+    public int pullingDownRopeSize = 1;
+    public float delayToPullDown = 1;
+
+    #endregion
+
+    private void Start()
+    {
+        rope = GetComponent<LineRenderer>();
+        attachedTo.transform.localPosition = new Vector3(0, ropeSize);                          //Set the position of the chest and therefore the rope height 
+    }
+
+    private void Update()
+    {
+        rope.SetPosition(1, attachedTo.transform.localPosition);                                //The rope is always attach to the chest
+
+        if (rope.GetPosition(1).y > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                attachedTo.transform.position -= new Vector3(0, -pullingUpRopeSize);            //Pulling up the chest
+            }
+            else
+            { 
+
+                if (level3)
+                {
+                    timer += Time.deltaTime;
+
+                    if (timer > delayToPullDown)
+                    {
+                        timer = 0;
+                        attachedTo.transform.position -= new Vector3(0, pullingDownRopeSize);   //Automatically pulling down the chest
+                    }
+                }              
+            }
+        }
+        else
+        {
+            if (!win)
+            {
+                win = true;
+                rope.SetPosition(1, new Vector3(0, 0));
+                GameManager.Instance.gameState = GameManager.GameState.Win;
+            }          
+        }    
+    }
+}
---
+++ WarioWare/Assets/MicroGames/Cluster Theodore/TrioLeRafiot/123Requin_01/Prefabs/Scripts/SharkSysteme.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkSysteme : MonoBehaviour
+{
+    #region Variable
+    public static SharkSysteme Instance;
+
+    [Header ("Temps que le shark reste")]
+    public float effectiveTime;
+
+    [Header ("Temps que le requin spawn après l'alerte")]
+    public float timeForSpawn;
+
+    [Header ("Attente min entre chaque alerte")]
+    public float timeBtwSpawn;
+
+    [Header ("Temps avant lancement premier spawn")]
+    public float minStartSpawn;
+
+    [Header ("Bool qui permet ou non l'apparition du shark")]
+    public bool lockCanSpawn;
+
+    [Header ("Bool qui detecte la presence du shark")]
+    public bool sharkIsHere;
+
+    [Space]
+    public GameObject shark;
+    public GameObject stopSign;
+    public Transform sharkPoint;
+
+    private GameObject actualShark;
+    private Animator signAnim;
+    #endregion
+
+    void Start()
+    {
+        ManagerInit();
+        signAnim = stopSign.GetComponent<Animator>();
+
+        lockCanSpawn = true;
+        sharkIsHere = false;
+
+        stopSign.SetActive(false);
+
+        StartCoroutine(StartTime());
+    }
+
+    void Update()
+    {
+        if (sharkIsHere == false)
+        {
+            if (lockCanSpawn == false)
+            {
+                lockCanSpawn = true;
+                StartCoroutine(SharkSpawnRandom());
+            }
+        }
+    }
+
+
+    // Permet d'avoir un obj de ce type
+    void ManagerInit()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Coroutine qui fait spawn le shark de manière aléatoire
+    IEnumerator SharkSpawnRandom()
+    {
+        float cooldown = Random.Range(1, 3);
+        yield return new WaitForSeconds(cooldown);
+
+        stopSign.SetActive(true);
+        yield return new WaitForSeconds(timeForSpawn);
+        signAnim.SetBool("switch", true);
+        actualShark = Instantiate(shark, sharkPoint.transform.position, sharkPoint.transform.rotation);
+
+        sharkIsHere = true;
+
+        StartCoroutine(LifeTime());
+    }
+
+    // Coroutine qui detruit le shark après un certain temps
+    IEnumerator LifeTime()
+    {
+        yield return new WaitForSeconds(effectiveTime);
+        Destroy(actualShark);
+
+        stopSign.SetActive(false);
+
+        sharkIsHere = false;
+
+        StartCoroutine(Cooldown());
+    }
+
+    // Cooldown entre chaque spawn
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(timeBtwSpawn);
+        lockCanSpawn = false;
+    }
+
+    // Cooldown du start
+    IEnumerator StartTime()
+    {
+        yield return new WaitForSeconds(minStartSpawn);
+        lockCanSpawn = false;
+    }
+}
