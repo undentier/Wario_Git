3d63e904661f5b8beb5022b8e30b8abb0057ba4e
Guillaume <g.roge@rubika-edu.com>
2020-12-14 12:10:31 +0100

    Planche Player done

---
+++ WarioWare/Assets/MicroGames/Cluster Theodore/TrioLeRafiot/Planche_03/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Testing;
+
+namespace LeRafiot
+{
+    namespace Planche
+    {
+        /// <summary>
+        /// Guillaume Rogé
+        /// This script control the player
+        /// </summary>
+
+        public class PlayerController : MonoBehaviour
+        {
+            public static PlayerController Instance;
+
+            #region Variable
+
+            [Header ("Rotation point")]
+            public Transform[] rotaPoint;
+
+            [Header ("Player value")]
+            public int playerState;
+            public bool canMove;
+
+            #endregion 
+
+            void Start()
+            {
+                ManagerInit();
+
+                canMove = true;
+                playerState = 2;
+            }
+
+    
+            void Update()
+            {
+                PlayerMouvement();
+            }
+
+
+            void ManagerInit()
+            {
+                if (Instance == null)
+                {
+                    Instance = this;
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+            void PlayerMouvement()
+            {
+                if (canMove)
+                {
+                    if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetButtonDown("Right_Bumper"))
+                    {
+
+                        if (playerState < 4)
+                        {
+                            gameObject.transform.rotation = rotaPoint[playerState + 1].transform.rotation;
+                            playerState++;
+                        }
+                        else
+                        {
+                            canMove = false;
+                            Manager.Instance.Result(false);
+                            SoundManagerPlanche.Instance.sfxSound[1].Play();
+                        }
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetButtonDown("Left_Bumper"))
+                    {
+
+                        if (playerState > 0)
+                        {
+                            gameObject.transform.rotation = rotaPoint[playerState - 1].transform.rotation;
+                            playerState--;
+                        }
+                        else
+                        {
+                            canMove = false
+                            Manager.Instance.Result(false);
+                            SoundManagerPlanche.Instance.sfxSound[1].Play();
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
